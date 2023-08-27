4526f12ac2efde802c4e6a6416d260911d18b1ac
Anirban Pramanik <122162797+AnirbanPramanik10@users.noreply.github.com>
2023-08-27 21:09:14 +0530

    Add files via upload

---
+++ SQL Querrys/Module1.sql
@@ -0,0 +1,112 @@
+-- DAY - 1
+
+-- Retreiving data from single table
+
+
+-- SELECT first_name, last_name, state, points , (points + 10) * 100 AS 'Discount factor'
+-- SELECT name , unit_price , unit_price * 1.1 AS 'new price'
+-- FROM products
+
+-- SELECT *
+-- FROM orders
+ -- WHERE order_date >= '2019-01-01'
+ 
+ -- SELECT *
+ -- FROM order_items
+ -- WHERE order_id = 6  AND quantity * unit_price > 30
+
+
+-- SELECT *
+-- FROM products
+-- WHERE quantity_in_stock IN ('49','38','72')
+
+-- SELECT *
+-- FROM customers
+-- WHERE address LIKE '%trail%' OR 
+	  -- address LIKE '%avenue%' OR
+      -- phone LIKE '%9'
+-- WHERE birth_date between '1990-01-01' AND '2000-01-01'
+
+
+-- SELECT *
+-- FROM customers
+-- WHERE first_name regexp 'elka|ambur' 
+-- WHERE last_name regexp 'EY$|ON$' 
+ -- WHERE     last_name regexp '^MY|SE' 
+  -- WHERE    last_name regexp 'b[r|u]'
+  
+  
+  
+ -- SELECT * 
+ -- FROM orders
+ -- WHERE shipped_date IS  null
+  
+  
+--  SELECT *, quantity * unit_price AS 'total_price'
+--  FROM order_items 
+--  WHERE order_id = 2
+--  ORDER BY total_price DESC
+  
+  
+  
+ -- SELECT * 
+ -- FROM customers 
+ -- ORDER BY points DESC
+ -- LIMIT 3
+  
+  
+  -- END of day 1 
+  
+  
+  
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
